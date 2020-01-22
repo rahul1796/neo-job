a6ac68df1952b3d42e8432d1c51d9398db0ada8c
CodeCalibro <akash.g@labournet.in>
2020-01-22 18:31:43 +0530

    JP-330 opportunity life cycle created

---
+++ application/controllers/CommercialVerificationController.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CommercialVerificationController extends MY_Controller {
+
+  public function commericalsStore() {
+    echo 'hello';
+  }
+
+}
