0e036daa116508663b060f0fe2a8f6ae304c93ed
CodeCalibro <akash.g@labournet.in>
2020-01-28 00:43:29 +0530

    JP-335 count changes

--- application/models/Dashboard.php
+++ application/models/Dashboard.php
@@ -24,6 +24,14 @@ class Dashboard extends MY_Model
     return $this->IND_money_format($this->db->select('*')->from('neo.candidates')->count_all_results());
   }
 
+  public function getCompanyCount() {
+    return $this->db->where_in('created_by', $this->hierarchy())->from('neo_customer.companies')->count_all_results();
+  }
+
+  public function getOpportunityCount() {
+    return $this->db->where_in('created_by', $this->hierarchy())->from('neo_customer.opportunities')->count_all_results();
+  }
+
   public function getEmployerCount() {
     //return $this->IND_money_format($this->db->where('is_customer', true)->from('neo_customer.customers')->count_all_results());
     return $this->db->where('is_customer', true)->where_in('created_by', $this->hierarchy())->from('neo_customer.customers')->count_all_results();
@@ -55,13 +63,13 @@ class Dashboard extends MY_Model
     }
     return $this->db->from('neo_job.jobs')->count_all_results();
   }
-  
+
   /******** SUMIT *************/
-  
-    public function getJobOpeningsCount() {      
+
+    public function getJobOpeningsCount() {
        //return $this->IND_money_format($this->db->select_sum('no_of_position::INTEGER')->where_in('created_by', $this->hierarchy())->where_in('job_status_id', [2])->from('neo_job.jobs')->get()->row('no_of_position'));
        $HierarchyIdList = join(",",$this->hierarchy());
-               
+
         $query = "WITH P AS
                 (
                     SELECT 	CP.job_id,
@@ -71,19 +79,19 @@ class Dashboard extends MY_Model
                     GROUP BY 	CP.job_id
                 )
                 SELECT      COALESCE(SUM(no_of_position::INTEGER),0) AS job_openings,
-                            COALESCE(SUM(P.joined_count),0) AS joined_count,			
-                            (CASE 
+                            COALESCE(SUM(P.joined_count),0) AS joined_count,
+                            (CASE
                                 WHEN COALESCE(SUM(P.joined_count),0)>COALESCE(SUM(no_of_position::INTEGER),0) THEN 0
-                                ELSE COALESCE(SUM(no_of_position::INTEGER),0)-COALESCE(SUM(P.joined_count),0) 
+                                ELSE COALESCE(SUM(no_of_position::INTEGER),0)-COALESCE(SUM(P.joined_count),0)
                             END) AS total_openings
                 FROM        neo_job.jobs AS J
                 LEFT JOIN   P ON P.job_id=J.id
                 WHERE       J.job_status_id=2
                 AND         J.created_by IN (".$HierarchyIdList.") ";
             $result = $this->db->query($query)->row()->total_openings;
-            return $result;       
+            return $result;
     }
-    
+
  /******* SUMIT END *************/
   public function getInterestedCandidatesCount() {
     return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('candidate_status_id', [1])->from('neo_job.candidates_jobs')->count_all_results());
@@ -112,7 +120,7 @@ class Dashboard extends MY_Model
     public function getJoinedCandidatesCount() {
         //return $this->db->where_in('created_by', $this->hierarchy())->where_in('candidate_status_id', [15])->from('neo_job.candidates_jobs')->count_all_results();
         $HierarchyIdList = join(",",$this->hierarchy());
-               
+
         $query = "WITH P AS
                 (
                     SELECT 	CP.job_id,
@@ -156,71 +164,79 @@ class Dashboard extends MY_Model
 
 
     public function getLeadIdentifiedCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [1])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [1])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getInitialMeetingScheduleCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [2])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [2])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getInitialMeetingCompletedCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [3])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [3])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getOpLostAtEntryLevelCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [4])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [4])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getFollowUpMeetingScheduleCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [5])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [5])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getFollowUpMeetingCompletedCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [6])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [6])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getOpLostAtFollowUpLevelCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [7])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [7])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getProposalSharedCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [8])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [8])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getProposalUnderReviewCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [9])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [9])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getProposalUnderRFECount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [10])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [10])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getNegotiationCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [11])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [11])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getProposalAcceptedCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [12])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [12])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getOpLostAtProposalLevelCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [13])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [13])->from('neo_customer.opportunities')->count_all_results());
     }
 
-    public function getContractSignedCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [14])->from('neo_customer.customers')->count_all_results());
+    public function getOpportunityLostCount() {
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [18])->from('neo_customer.opportunities')->count_all_results());
     }
 
-    public function getContractNotSignedCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [15])->from('neo_customer.customers')->count_all_results());
+    public function getLegalApprovedCount() {
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [20])->from('neo_customer.opportunities')->count_all_results());
+    }
+
+    public function getLegalRejectedCount() {
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [21])->from('neo_customer.opportunities')->count_all_results());
+    }
+
+    public function getContractCompletedCount() {
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [22])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getLeadConvertToClientCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [16])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [16])->from('neo_customer.opportunities')->count_all_results());
     }
 
     public function getOnHoldCount() {
-      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [17])->from('neo_customer.customers')->count_all_results());
+      return $this->IND_money_format($this->db->where_in('created_by', $this->hierarchy())->where_in('lead_status_id', [17])->from('neo_customer.opportunities')->count_all_results());
     }
 
   private function IND_money_format($number)
